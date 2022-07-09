f0de9f283969c48ff79c284217f3daeaa416c8df
Masri Manas <saya@masrimanas.com>
2022-07-09 14:09:48 +0700

    edit home page #1

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
-import { h } from "preact";
-import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+import { h } from 'preact';
+import { tw } from '@twind';
+import Counter from '../islands/Counter.tsx';
 
 export default function Home() {
   return (
@@ -11,6 +11,7 @@ export default function Home() {
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
+      <a href="https://manas.deno.dev/about">About me</a>
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
         file, and refresh.
