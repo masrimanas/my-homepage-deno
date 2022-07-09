507146d6f3f5ab75992f08d8337248bd6a74b269
Masri Manas <saya@masrimanas.com>
2022-07-09 13:58:14 +0700

    adding about page

---
+++ routes/about.tsx
@@ -0,0 +1,20 @@
+/** @jsx h */
+import { h } from 'preact';
+import { tw } from '@twind';
+// import Counter from '../islands/Counter.tsx';
+
+export default function Home() {
+  return (
+    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+      <img
+        src="/logo.svg"
+        height="100px"
+        alt="the fresh logo: a sliced lemon dripping with juice"
+      />
+      <p class={tw`my-6`}>
+        I am a former English Teacher who is currently trying to pursue my
+        childhood dream to become a programmer.
+      </p>
+    </div>
+  );
+}
